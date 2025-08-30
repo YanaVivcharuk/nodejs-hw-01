b893623fac8798ee352dcdf648022a3d37096bb7
YanaVivcharuk <yanavivcharuk@icloud.com>
2025-08-30 22:57:34 +0200

    first-node

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,11 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const removeLastContact = async () => {
+  const contacts = await readContacts();
+  contacts.pop();
+  await writeContacts(contacts);
+  console.log('Last contact removed.');
+};
 
 removeLastContact();
